7681fcc97dc104660d4b24109d8e45fd121effb9
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-11-30 15:02:16 +0300

    lab finished

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400){
+    return 'This one is on me!';
+  }else if (ride > 2000 && ride <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else if(ride > 2500){
+    return 'No can do.';
+  }
+}
+console.log(scuberGreetingForFeet(200))
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city == 'NYC'? 'Ok, sounds good.' : 'No go.');
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip) {
+    case "generous":
+      return ("Thank you so much.")
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+      break;
+    case "not as generous":
+      return ("Thank you.")
+      break;
+    default: 
+      return('Bye.')
+  }
 }
